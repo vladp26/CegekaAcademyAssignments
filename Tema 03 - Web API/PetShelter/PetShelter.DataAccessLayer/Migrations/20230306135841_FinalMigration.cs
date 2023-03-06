e060aff9d3ea7c2973c6f38b3280e421283285fd
vladp26 <95245952+vladp26@users.noreply.github.com>
2023-03-06 16:05:15 +0200

    Tema 03 - Added Migrations Folder

---
+++ Tema 03 - Web API/PetShelter/PetShelter.DataAccessLayer/Migrations/20230306135841_FinalMigration.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShelter.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DonorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fundraisers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: false),
+                    GoalValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0m),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 4, 5, 15, 58, 40, 936, DateTimeKind.Local).AddTicks(856)),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false, defaultValue: "Active"),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 3, 6, 15, 58, 40, 936, DateTimeKind.Local).AddTicks(1422)),
+                    DonationAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0m)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fundraisers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IdNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FundraiserWhichGotTheDonationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Fundraisers_FundraiserWhichGotTheDonationId",
+                        column: x => x.FundraiserWhichGotTheDonationId,
+                        principalTable: "Fundraisers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsHealthy = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    WeightInKg = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsSheltered = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    RescuerId = table.Column<int>(type: "int", nullable: true),
+                    AdopterId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pets_Persons_AdopterId",
+                        column: x => x.AdopterId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Pets_Persons_RescuerId",
+                        column: x => x.RescuerId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donations_DonorId",
+                table: "Donations",
+                column: "DonorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fundraisers_OwnerId",
+                table: "Fundraisers",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_FundraiserWhichGotTheDonationId",
+                table: "Persons",
+                column: "FundraiserWhichGotTheDonationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_AdopterId",
+                table: "Pets",
+                column: "AdopterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_RescuerId",
+                table: "Pets",
+                column: "RescuerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donations_Persons_DonorId",
+                table: "Donations",
+                column: "DonorId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Fundraisers_Persons_OwnerId",
+                table: "Fundraisers",
+                column: "OwnerId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Fundraisers_Persons_OwnerId",
+                table: "Fundraisers");
+
+            migrationBuilder.DropTable(
+                name: "Donations");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Fundraisers");
+        }
+    }
+}
